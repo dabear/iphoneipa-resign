6a90038dff14d5da49a56599a37b1509377b3380
dabear <bjorninge@bjorninge.no>
2017-07-25 09:35:24 +0200

    initial command line version

---
+++ resign.sh
@@ -0,0 +1,177 @@
+#/bin/bash
+#sudo pip install iResign
+
+
+function realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+
+
+function create_temp {
+    export TEMPDIR=$(mktemp -d -t iosxdripreader)
+    mkdir "$TEMPDIR/app"
+    mkdir "$TEMPDIR/provisioning"
+}
+
+function remove_temp {
+  if [ -d "$TEMPDIR" ]; then
+    rm -rf $TEMPDIR
+  fi
+}
+
+function unpackage_ipa {
+  ipa_path=$(realpath $1)
+  export IPA_BASE=$(basename "$ipa_path")
+  export IPA_DIR=$(dirname "$ipa_path")
+  unzip "$ipa_path" -d "$TEMPDIR/app"
+  
+
+}
+
+function copy_app_provision_profile {
+  profile=$1
+  if [ -f "$profile" ]; then
+    cp $profile $TEMPDIR/provisioning
+  fi
+
+}
+
+function find_app_within_unpackaged_ipa {
+  res=$(find "$TEMPDIR" -name *.app | head -1)
+  echo $res
+
+}
+
+function get_provision_profile {
+  res=$(find "$TEMPDIR/provisioning" -name *.mobileprovision | head -1)
+  echo $res
+}
+
+function resign_app {
+  #sign_identity="iPhone Developer"
+  sign_identity=$1
+  app_path=$(find_app_within_unpackaged_ipa)
+  if [ -d "$app_path" ]; then
+    profile=$(get_provision_profile)
+    if [ -f "$profile" ]; then
+        echo found profile
+        iresign.py -v "$app_path" "$profile" "$sign_identity" 
+    else
+        echo "ERROR: could not find provisioning profile, aborting resign!"
+        exit -2
+    fi
+  else
+    echo "ERROR: aborting, could not find app folder within .ipa!"
+    #exit -1
+  fi
+
+}
+
+function verify_sign_identity {
+    sign_identity=$1
+    matching_ids_count=$(security find-identity -v -p codesigning | grep $sign_identity | wc -l)
+    echo $matching_ids_count
+}
+
+
+function get_app_signature {
+    app_path=$(find_app_within_unpackaged_ipa)
+    if [ -d "$app_path" ]; then
+        res=$(codesign -dv "$app_path" 2>&1)
+        signtime=$(echo "$res" | awk -F "=" '/Signed\ Time/ {print $2}')
+        teamid=$(echo "$res" | awk -F "=" '/TeamIdentifier/ {print $2}')
+        echo "Team $teamid (signtime: $signtime)"
+    else
+        echo "none (none)"
+    fi 
+}
+
+function repackage_app {
+    newfilename="$IPA_DIR/resigned-$IPA_BASE"
+    pushd "$TEMPDIR/app/" &>/dev/null
+    zip -r -X "$newfilename" "Payload" &>/dev/null
+    popd &>/dev/null
+    echo $newfilename
+}
+
+
+function apphelp {
+    echo "Usage: resign.sh <ipafile> <mobileprovisionfile>"
+    echo "Example: ./resign.sh $HOME/Downloads/HelpDiabetes.ipa $HOME/Downloads/bjorningewildcard.mobileprovision"
+    echo "Instructions for generating/downloading mobileprovisionfiles can be found online at the following location: https://calvium.com/how-to-make-a-mobileprovision-file/"
+}
+
+function guard_is_developer {
+    set -x
+    devid=$1
+    signing_id_count=$(security find-identity -v -p codesigning | grep $devid | wc -l)
+    if [ "$signing_id_count" -eq "0" ]; then
+        echo "ERROR: You don't appear to have $devid signing certificates installed, aborting!";
+        exit -5;
+    fi
+    
+}
+
+#ipafile=oldiosxdripreader-kopi.ipa
+developer_id="iPhone Developer"
+ipafile=$1
+provisionfile=$2
+
+create_temp
+trap remove_temp EXIT
+
+
+if [ "$#" -ne 2 ]; then
+    apphelp
+    exit 0
+fi
+
+if [ $1 = "--help" ]; then
+   apphelp
+   exit 0
+
+fi
+
+if [ $1 = "-h" ]; then
+   apphelp
+   exit 0
+
+fi
+
+guard_is_developer $developer_id
+
+if [ ! -f "$provisionfile" ]; then
+    echo ERROR: mobileprovisionfile not found
+    exit -2
+fi
+
+if [ -f "$ipafile" ]; then
+    unpackage_ipa "$ipafile"
+    signature_before=$(get_app_signature)
+    copy_app_provision_profile "$provisionfile"
+    resign_app "$developer_id"
+    signature_after=$(get_app_signature)
+    
+    newipa=$(repackage_app)
+    if [ -f "$newipa" ]; then
+        echo "Signature changed from $signature_before to $signature_after and newipa created: $newipa"
+    else
+        echo "ERROR: new ipa could not be created"
+    fi 
+    
+    
+else
+    echo "ERROR:    Could not locate ipafile $ipafile, aborting!"
+    exit -3
+fi
+
+
