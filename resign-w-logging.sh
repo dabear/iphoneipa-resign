dd51421781151de9d1cbcebf0d1b1a34a11ca84c
dabear <bjorninge@bjorninge.no>
2017-07-25 21:08:41 +0200

    move to add logging to file in the  same dir ipafile

--- resign-w-logging.sh
+++ resign-w-logging.sh
@@ -1,2 +1,25 @@
 #/bin/bash
-./resign.sh "$@" &> $HOME/resignlog.txt
+function realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+
+ipafile=$1
+if [[ $2 =~ .*\.ipa$ ]]; then
+    ipafile=$2
+fi
+
+if [ -f "$ipafile" ]; then
+    logdir=$(dirname $(realpath "$ipafile"))
+else
+    logdir="$HOME"
+fi
+./resign.sh "$@" &> "$logdir/resignlog.txt"
